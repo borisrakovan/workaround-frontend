01362066083a459469bf55ab7856c3f6d5f3a25b
Rakovan, Boris <rakovbor@fel.cvut.cz>
2021-10-16 14:37:40 +0200

    integrate tailwind

---
+++ src/WorkaroundLogo.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material"
+import React from "react"
+
+interface Props {}
+
+const WorkaroundLogo = (props: Props) => {
+   return (
+      <Typography sx={{ mt: 6, mb: 3 }} color="text.secondary">
+         workaround
+      </Typography>
+   )
+}
+
+export default WorkaroundLogo
