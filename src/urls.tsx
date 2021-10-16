8940d72fd6fba9a3b420b79f42ff208505b7df86
Jakub Jun <jakubjun21@gmail.com>
2021-10-16 16:01:27 +0200

    Change backend URLs

--- src/urls.tsx
+++ src/urls.tsx
@@ -1,4 +1,6 @@
-export const BACKEND_URL =
-   process.env.NODE_ENV === "production"
-      ? "http://localhost:8000"
-      : "http://localhost:8000"
+export const BACKEND_URL = "http://10.12.202.69:8000/"
+
+// export const BACKEND_URL =
+//    process.env.NODE_ENV === "production"
+//       ? "http://localhost:8000"
+//       : "http://localhost:8000"
